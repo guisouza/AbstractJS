6e98b0f95942b58b846360a30c8985c0861dfa82
Guilherme (Coder) de Souza <gui_souza@me.com>
2014-09-06 15:34:35 -0300

    forgot to put new files in the last commit ..  sorry for that

---
+++ src/core/coreMethods/x.core.repeatIterator.js
@@ -0,0 +1,27 @@
+//File : src/core/coreMethods/x.core.repeatIterator.js
+
+(function(x){
+	'use strict';
+	x.core.repeatIterator = function(statement,data,template,element,controller){
+
+    var tmplVars = template.match(/\{\{.*\}\}/g);
+    var result = '';
+
+    controller[data].forEach(function(data,index){
+      var iteration = template;
+      data.$ = index;
+      for(var field in tmplVars){
+        var tmplVar = tmplVars[field].replace(/}}|{{/g,'');
+        iteration = iteration.replace(tmplVars[field],x.core.extractField(tmplVar,data));
+      }
+
+      result = result+iteration;
+    });
+
+    element.innerHTML = result;
+
+    x.core.parse('events',element,controller);
+	};
+
+
+})(this.x);
